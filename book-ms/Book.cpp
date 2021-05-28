d68e7683642ec78066bf881e4eb4d1efb6357b3b
superburn <18800109197@163.com>
2021-05-28 17:33:27 +0800

    添加项目文件。

---
+++ book-ms/Book.cpp
@@ -0,0 +1,135 @@
+#include "Book.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+//#include <iomanip>
+
+using namespace std;
+
+CBook::CBook(char* cName, char* cIsbn, char* cPrice, char* cAuthor) {
+    strncpy(m_cname, cName, NUM1);
+    strncpy(m_cIsbn, cIsbn, NUM1);
+    strncpy(m_cPrice, cPrice, NUM2);
+    strncpy(m_cAuthor, cAuthor, NUM2);
+}
+
+char* CBook::GetName() {
+    return m_cname;
+}
+
+void CBook::SetName(char* cName) {
+    strncpy(m_cname, cName, NUM1);
+}
+
+char* CBook::GetIsbn() {
+    return m_cIsbn;
+}
+
+void CBook::SetIsbn(char* cIsbn) {
+    strncpy(m_cIsbn, cIsbn, NUM1);
+}
+
+char* CBook::GetPrice() {
+    return m_cPrice;
+}
+
+void CBook::SetPrice(char* cPrice) {
+    strncpy(m_cPrice, cPrice, NUM2);
+}
+
+char* CBook::GetAuthor() {
+    return m_cAuthor;
+}
+
+void CBook::SetAuthor(char* cAuthor) {
+    strncpy(m_cAuthor, cAuthor, NUM2);
+}
+
+//将图书对象写入到文件中
+void CBook::WriteData() {
+    ofstream ofs;
+    ofs.open("/Users/heshibo01/book.txt", ios::binary | ios::app);
+    try {
+        ofs.write(m_cname, NUM1);
+        ofs.write(m_cIsbn, NUM1);
+        ofs.write(m_cPrice, NUM2);
+        ofs.write(m_cAuthor, NUM2);
+    }
+    catch (...) {
+        throw "file error occurred";
+        ofs.close();
+    }
+    ofs.close();
+}
+
+//从文件中读取数据来构建对象
+void CBook::GetBookFromFile(int iCount) {
+    ifstream ifs;
+    ifs.open("/Users/heshibo01/book.txt", ios::binary);
+    char cName[NUM1];
+    char cIsbn[NUM1];
+    char cPrice[NUM2];
+    char cAuthor[NUM2];
+    try {
+        ifs.seekg(iCount * (NUM1 + NUM1 + NUM2 + NUM2), ios::beg);
+        ifs.read(cName, NUM1);
+        if (ifs.tellg() > 0) {
+            strncpy(m_cname, cName, NUM1);
+        }
+        ifs.read(cIsbn, NUM1);
+        if (ifs.tellg() > 0) {
+            strncpy(m_cIsbn, cIsbn, NUM1);
+        }
+        ifs.read(cPrice, NUM2);
+        if (ifs.tellg() > 0) {
+            strncpy(m_cPrice, cPrice, NUM2);
+        }
+        ifs.read(cAuthor, NUM2);
+        if (ifs.tellg() > 0) {
+            strncpy(m_cAuthor, cAuthor, NUM2);
+        }
+    }
+    catch (...) {
+        throw "file error occurred";
+        ifs.close();
+    }
+    ifs.close();
+}
+
+//将图书信息从文件中删除
+void CBook::DeleteData(int iCount) {
+    long resPos;
+    int iDataCount = 0;
+    fstream file;
+    fstream tempFile;
+    ofstream ofile;
+    char cTempBuf[NUM1 + NUM1 + NUM2 + NUM2];
+    file.open("/Users/heshibo01/book.txt", ios::binary | ios::in | ios::out);
+    tempFile.open("/Users/heshibo01/temp.txt", ios::binary | ios::in | ios::out | ios::trunc);
+    file.seekg(0, ios::end);
+    resPos = file.tellg();
+    iDataCount = resPos / (NUM1 + NUM1 + NUM2 + NUM2);
+    if (iCount < 0 || iCount > iDataCount) {
+        throw "Input number error";
+    }
+    else {
+        file.seekg((iCount) * (NUM1 + NUM1 + NUM2 + NUM2), ios::beg);
+        for (int j = 0; j < (iDataCount - iCount); j++) {
+            memset(cTempBuf, 0, NUM1 + NUM1 + NUM2 + NUM2);
+            file.read(cTempBuf, NUM1 + NUM1 + NUM2 + NUM2);
+            tempFile.write(cTempBuf, NUM1 + NUM1 + NUM2 + NUM2);
+        }
+        file.close();
+        tempFile.seekg(0, ios::beg);
+        ofile.open("/Users/heshibo01/book.txt");
+        ofile.seekp((iCount - 1) * (NUM1 + NUM1 + NUM2 + NUM2), ios::beg);
+        for (int i = 0; i < (iDataCount - iCount); i++) {
+            memset(cTempBuf, 0, NUM1 + NUM1 + NUM2 + NUM2);
+            tempFile.read(cTempBuf, NUM1 + NUM1 + NUM2 + NUM2);
+            ofile.write(cTempBuf, NUM1 + NUM1 + NUM2 + NUM2);
+        }
+    }
+    tempFile.close();
+    ofile.close();
+    remove("temp.txt");
+}
---
+++ book-ms/book-ms.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+#include <conio.h>
+#include <fstream>
+#include "Book.h"
+
+#define CMD_COLS 90
+#define CMD_LINES 25
+
+using namespace std;
+
+int main()
+{
+    
+}
+
+void SetScreenGrid(){
+    char sysSetBuf[80];
+    sprintf(sysSetBuf, "mode con cols=%d lines=%d", CMD_COLS, CMD_LINES);
+    system(sysSetBuf);
+}
+
+void SetSysCaption() {
+    system("title Sample");
+}
+
+void ClearScreen() {
+    system("cls");
+}
+
+void SetSysCaption(const char* pText) {
+    char sysSetBuf[80];
+    sprintf(sysSetBuf, "title %s", pText);
+    system(sysSetBuf);
+}
+
+void ShowWelcome() {
+    for (int i = 0; i < 7; i++) {
+        cout << endl;
+    }
+    cout << setw(40);
+    cout << "**************" << endl;
+    cout << setw(40);
+    cout << "图书管理系统" << endl;
+    cout << setw(40);
+    cout << "**************" << endl;
+}
+
+void ShowRootMenu() {
+    cout << setw(40);
+    cout << "请选择功能" << endl;
+    cout << endl;
+    cout << setw(38);
+    cout << "1 添加新书" << endl;
+    cout << endl;
+    cout << setw(38);
+    cout << "2 浏览全部" << endl;
+    cout << endl;
+    cout << setw(38);
+    cout << "3 删除图书" << endl;
+}
+
+void WaitUser() {
+    int iInputPage = 0;
+    cout << "enter：返回主菜单 q:退出" << endl;
+    char buf[256];
+    gets_s(buf);
+    if (buf[0] == 'q') {
+        system("exit");
+    }
+}
+
+void mainloop() {
+    ShowWelcome();
+    while (1) {
+        ClearScreen();
+        ShowWelcome();
+        ShowRootMenu();
+        switch (GetSelect())
+        {
+        case 1:
+            ClearScreen();
+            //GuideInput();
+            break;
+        case 2:
+            ClearScreen();
+            //ViewData();
+            break;
+        case 3:
+            ClearScreen();
+            //DeleteBookFromFile();
+            break;
+        }
+    }
+}
+
+int GetSelect() {
+    char buf[256];
+    gets_s(buf);
+    return atoi(buf);
+}
